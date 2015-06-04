49685df6db340186213aaece5f9157b97c89b66a
Pomin Wu <pomin5@gmail.com>
2015-06-04 11:52:43 +0800

    edit contents

---
+++ src/zh-tw/sponsors/index.md
@@ -0,0 +1,32 @@
+---
+layout: nosides.php
+---
+# 大會贊助商
+
+## 鑽石級贊助
+
+（準備中）
+
+## 黃金級贊助
+
+（準備中）
+
+## 白銀級贊助
+
+[![](/assets/images/sponsor-archilife.jpg)](http://www.archilife.org/)
+
+## 青銅級贊助
+
+[![](/assets/images/sponsor-wabow.jpg)](http://www.wabow.com/)
+
+## 協辦單位
+
+（準備中）
+
+## 媒體夥伴
+
+（準備中）
+
+## 贊助 COSCUP
+
+如果您欲贊助 COSCUP，請與 <a href="mailto:sponsorship@coscup.org">sponsorship@coscup.org</a> 聯絡。
--- theme/marksite/events.php
+++ theme/marksite/events.php
@@ -7,29 +7,7 @@
       </ul>
     </div>
     <div class="content">
-      COSCUP 十週年了！台灣的開源社群不斷茁壯、參與者越來越多元，近一兩年，開放的精神更是擴散到程式碼之外的領域，替社會注入一股新氣象。COSCUP 2015 的主軸即是「開放文化」，來吧！熱愛開放與自由的你，一起來讓開源精神散播到更廣大的世界吧！
-    </div>
-  </section>
-  <section class="workshop feature block">
-    <div class="title">
-      工作坊
-      <ul class="menu">
-        <li><a href="http://coscup2015.kktix.cc/events/workshop2015">報名</a></li>
-      </ul>
-    </div>
-    <div class="content">
-      最熱血的夏天！最 Open 的活動！15個社群，15場工作坊，台灣最大的開源盛會！
-    </div>
-  </section>
-  <section class="film feature block">
-    <div class="title">
-      影展
-      <ul class="menu">
-        <li><a href="#">敬請期待</a></li>
-      </ul>
-    </div>
-    <div class="content">
-      今年 COSCUP 還規劃了開源文化相關的電影！一起來看電影吧！
+      COSCUP 十週年了！台灣的開源社群不斷茁壯、參與者越來越多元，近一兩年，開放的精神更是擴散到程式碼之外的領域。COSCUP 2015 的主軸即是「開放文化」，來吧！熱愛開放與自由的你，一起來讓開源精神散播到更廣大的世界吧！
     </div>
   </section>
   <section class="conference feature block">
@@ -40,7 +18,29 @@
       </ul>
     </div>
     <div class="content">
-      COSCUP 研討會已堂堂邁入第十年。十年來不但由各地社群共同舉辦，在眾多同好的參與之下，逐漸成長茁壯。無論你是寫 open source 軟體的開發者、熱衷 open source 軟體的推廣者、熟悉 open source 軟體的使用者或是想了解 open source 軟體的新手，都歡迎你來參加一年一度全台最大的開源人年會。
+      最熱血的夏天！最 Open 的活動！台灣最大的開源盛會 COSCUP 研討會已堂堂邁入第十年。無論你是寫開源軟體的開發者、熱衷開源軟體的推廣者、熟悉開源軟體的使用者或是想了解開源軟體的新手，都歡迎你來參加一年一度全台最大的開源人年會。
+    </div>
+  </section>
+  <section class="workshop feature block">
+    <div class="title">
+      工作坊
+      <ul class="menu">
+        <li><a href="http://coscup2015.kktix.cc/events/workshop2015">報名</a></li>
+      </ul>
+    </div>
+    <div class="content">
+      參與 COSCUP 除了聊開源、交朋友，更要動手做！來自 15 個社群，15 場工作坊，讓你在實作中渡過最熱血的夏天！
+    </div>
+  </section>
+  <section class="film feature block">
+    <div class="title">
+      開放文化影展
+      <ul class="menu">
+        <li><a href="#"></a></li>
+      </ul>
+    </div>
+    <div class="content">
+      開放不只是開源軟體！COSCUP 安排了 12 部與開放文化相關、未在台灣上映的電影，還有電影討論會，讓你即使不懂寫程式，也能看電影深入瞭解開放文化！
     </div>
   </section>
 </div>
