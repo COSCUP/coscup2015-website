c5d2f4506656d0ab8b17abde6724686dc7e0336e
Pomin Wu <pomin5@gmail.com>
2015-06-04 10:20:15 +0800

    fixx link

--- theme/marksite/navbar.php
+++ theme/marksite/navbar.php
@@ -2,7 +2,7 @@
   <ul class="main menu">
     <li><a href="<?php echo $current_path;?>/schedule">議程</a></li>
     <li><a href="<?php echo $current_path;?>/about">簡介</a></li>
-    <li><a href="<?php echo $current_path;?>/sponsor">贊助</a></li>
+    <li><a href="<?php echo $current_path;?>/sponsors">贊助</a></li>
     <li><a href="<?php echo $current_path;?>/">部落格</a></li>
   </ul>
 </nav>
