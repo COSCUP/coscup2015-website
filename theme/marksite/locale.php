f3da4bcdea8e796bab767f79d06e873bef1c6702
Pomin Wu <pomin5@gmail.com>
2015-06-07 15:26:22 +0800

    add en version

--- theme/marksite/locale.php
+++ theme/marksite/locale.php
@@ -1,5 +1,4 @@
 <ul class="locale menu">
-<!--  <li><a href="#">EN</a></li>-->
-<!--  <li><a href="#">正體</a></li>-->
-<!--  <li><a href="#">简体</a></li>-->
+  <li><a href="<?php echo $home_path;?>/en">EN</a></li>
+  <li><a href="<?php echo $home_path;?>/zh-tw">正體</a></li>
 </ul>
