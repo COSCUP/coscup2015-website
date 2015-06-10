9b9b64469e819e93e3bf8135aeca2a2b7e575358
Pomin Wu <pomin5@gmail.com>
2015-06-11 02:44:26 +0800

    add en content

---
+++ src/en/sponsors/index.md
@@ -0,0 +1,25 @@
+---
+layout: nosides.php
+---
+# Sponsors
+
+## Diamond
+
+## Gold
+
+## Silver
+
+[![](/2015/assets/images/sponsor-archilife.jpg)](http://www.archilife.org/)
+
+## Bronze
+
+[![](/2015/assets/images/sponsor-wabow.jpg)](http://www.wabow.com/)
+
+## Co-host
+
+
+## Media
+
+## Sponsorship
+
+If you are interested in sponsoring COSCUP, please contact <a href="mailto:sponsorship@coscup.org">sponsorship@coscup.org</a>.
--- theme/marksite/events.php
+++ theme/marksite/events.php
@@ -1,46 +1,42 @@
 <div id="events">
   <section class="booking feature block">
-    <div class="title">
-      報名
+    <div class="title" >
+      <span data-l10n-id="registrate"></span>
       <ul class="menu">
-        <li><a href="#">尚未開放報名</a></li>
+        <li><a href="#" data-l10n-id="countdownWarning"></a></li>
       </ul>
     </div>
-    <div class="content">
-      COSCUP 十週年了！臺灣的開源社群不斷茁壯，也有越來越多不同背景的參與者跨入開源的領域，分享並交流彼此的理念和成果。最近的這兩年，開放精神更跨出了資訊領域，在社會的各處發揚體現。「開放文化」是我們為 COSCUP 2015 訂下的主軸，熱愛自由與開放的你，請和我們一起散播開源精神吧！
+    <div class="content" data-l10n-id="registrateContent">
     </div>
   </section>
   <section class="conference feature block">
     <div class="title">
-      大會
+      <span data-l10n-id="conference"></span>
       <ul class="menu">
         <li><a href="#"></a></li>
       </ul>
     </div>
-    <div class="content">
-      最熱血的夏天！最 Open 的活動！台灣最大的開源盛會 COSCUP 研討會已堂堂邁入第十年。無論你是寫開源軟體的開發者、熱衷開源軟體的推廣者、熟悉開源軟體的使用者或是想了解開源軟體的新手，都歡迎你來參加一年一度全台最大的開源人年會。
+    <div class="content" data-l10n-id="conferenceContent">
     </div>
   </section>
   <section class="workshop feature block">
     <div class="title">
-      工作坊
+      <span data-l10n-id="workshop"></span>
       <ul class="menu">
-        <li><a href="http://coscup2015.kktix.cc/events/workshop2015">報名</a></li>
+        <li><a href="http://coscup2015.kktix.cc/events/workshop2015" data-l10n-id="workshopRegister"></a></li>
       </ul>
     </div>
-    <div class="content">
-      在 COSCUP，除了聊開源、交朋友，更要動手做！來自 15 個社群，15 場工作坊，讓你在實作中渡過最熱血的夏天！
+    <div class="content" data-l10n-id="workshopContent">
     </div>
   </section>
   <section class="film feature block">
     <div class="title">
-      開放文化影展
+      <span data-l10n-id="film"></span>
       <ul class="menu">
         <li><a href="#"></a></li>
       </ul>
     </div>
-    <div class="content">
-      開放不只是開源軟體！COSCUP 安排了 12 部與開放文化相關、未在台灣上映的電影，還有電影討論會，讓你即使不懂寫程式，也能看電影一窺開放文化的面貌！
+    <div class="content" data-l10n-id="filmContent">
     </div>
   </section>
 </div>
