e6559d674032f469563f5983d1d4b71492f93a37
Po-chiang Chao <bobchao@gmail.com>
2015-07-03 20:14:16 +0800

    ... 完全不明白為何本來要改成那樣

--- theme/assets/js/context.js
+++ theme/assets/js/context.js
@@ -1,17 +1,16 @@
 define(['jquery'], function($) {
 
   var path = window.location.href.match(/2015\/([-\w]+)\/?/);
-  var lang = ((path && path[1]) || 'zh-tw').toLowerCase();
+  var lang = ((path && path[1]) || 'zh-tw')
   lang = lang.substr(0, 3) + lang.substr(3).toUpperCase();
 
   $(document).ready(function () {
     document.l10n.requestLocales(lang);
   })
 
-  //包在 ready 裡面似乎傳回時機不對會造成錯誤？
   return {
     origin: 'http://coscup.org',
     api_path: '/2015/api',
-    lang: lang
+    lang: lang.toLowerCase()
   }
 });
