1503348f4074d92ecb7bef2820e0ebf01f10b675
Pomin Wu <pomin5@gmail.com>
2015-07-05 13:08:45 +0800

    add missing ending HTML tag

--- theme/marksite/sponsor.php
+++ theme/marksite/sponsor.php
@@ -1,5 +1,5 @@
 <aside class="right aside sponsers roll">
-<div id="sponsor" class="empty">
+  <div id="sponsor" class="empty"></div>
   <h2 data-l10n-id="sponsorship">贊助 COSCUP</h2>
 <p data-l10n-id="mailtoSponsor">如果您欲贊助 COSCUP&#65292;請與 <a href='mailto:sponsorship@coscup.org'>sponsorship@coscup.org</a> 聯絡&#12290;</p>
 </aside>
